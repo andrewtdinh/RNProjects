eec44fc35689bb412fd7ba8f7c951c38518fee7c
Andrew Dinh <adinh@sightmachine.com>
2019-11-06 22:44:11 -0800

    Display two text elements, one beneath the other on screen

--- rn-react/src/screens/ComponentsScreen.js
+++ rn-react/src/screens/ComponentsScreen.js
@@ -2,14 +2,21 @@ import React from 'react'
 import { Text, StyleSheet } from 'react-native'
 
 const ComponentsScreen = () => {
+  const myName = "Andrew Dinh"
   return (
-    <Text style={styles.textStyle}>This is my components screen</Text>
+    <>
+      <Text style={styles.headerStyle}>Getting started with react native</Text>
+      <Text style={styles.subheaderStyle}>My name is {myName}</Text>
+    </>
   )
 };
 
 const styles = StyleSheet.create({
-  textStyle: {
-    fontSize: 30
+  headerStyle: {
+    fontSize: 45
+  }, 
+  subheaderStyle: {
+    fontSize: 20
   }
 });
 
