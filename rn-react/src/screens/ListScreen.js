ceb935d3965a380652ccdac6a506a1739d3877e6
Andrew Dinh <adinh@sightmachine.com>
2019-11-06 23:17:42 -0800

    Render out a FlatList inside the ListScreen component

--- rn-react/src/screens/ListScreen.js
+++ rn-react/src/screens/ListScreen.js
@@ -1,9 +1,27 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
 
 const ListScreen = () => {
+  const options = [
+    { label: 'option #1'},
+    { label: 'option #2'},
+    { label: 'option #3'},
+    { label: 'option #4'},
+    { label: 'option #5'},
+    { label: 'option #6'},
+    { label: 'option #7'},
+    { label: 'option #8'},
+    { label: 'option #9'},
+  ];
+
+
   return (
-    <Text>List Screen</Text>
+    <FlatList 
+      data={options}
+      renderItem={({ item }) => {
+        return <Text>{item.label}</Text>
+      }}
+    />
   )
 }
 
