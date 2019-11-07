57b4af992e5edd5010870028b05e01a85337e263
Andrew Dinh <adinh@sightmachine.com>
2019-11-06 22:46:07 -0800

    Use View component instead of React Fragmenton page

--- rn-react/src/screens/ComponentsScreen.js
+++ rn-react/src/screens/ComponentsScreen.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Text, StyleSheet } from 'react-native'
+import { Text, StyleSheet, View } from 'react-native'
 
 const ComponentsScreen = () => {
   const myName = "Andrew Dinh"
   return (
-    <>
+    <View>
       <Text style={styles.headerStyle}>Getting started with react native</Text>
       <Text style={styles.subheaderStyle}>My name is {myName}</Text>
-    </>
+    </View>
   )
 };
 
