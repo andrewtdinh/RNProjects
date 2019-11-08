addcf8b027ac4fbfd7cee5c6b07e2cf41d58b2fb
Andrew Dinh <adinh@sightmachine.com>
2019-11-07 23:11:48 -0800

    Add age information for each friend

--- rn-react/src/screens/ListScreen.js
+++ rn-react/src/screens/ListScreen.js
@@ -3,15 +3,15 @@ import { View, Text, StyleSheet, FlatList } from 'react-native'
 
 const ListScreen = () => {
   const friends = [
-    { name: 'Friend 1'},
-    { name: 'Friend 2'},
-    { name: 'Friend 3'},
-    { name: 'Friend 4'},
-    { name: 'Friend 5'},
-    { name: 'Friend 6'},
-    { name: 'Friend 7'},
-    { name: 'Friend 8'},
-    { name: 'Friend 9'},
+    { name: 'Friend 1', age: 3},
+    { name: 'Friend 2', age: 24},
+    { name: 'Friend 3', age: 67},
+    { name: 'Friend 4', age: 23},
+    { name: 'Friend 5', age: 18},
+    { name: 'Friend 6', age: 32},
+    { name: 'Friend 7', age: 56},
+    { name: 'Friend 8', age: 45},
+    { name: 'Friend 9', age: 4},
   ];
 
 
