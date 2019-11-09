7fe41b985c8b85282fd64a3139d9748777df7312
Andrew Dinh <adinh@sightmachine.com>
2019-11-08 19:44:57 -0800

    Use React Native Navigation to navigate to respective screen upon button touch

--- rn-react/src/screens/HomeScreen.js
+++ rn-react/src/screens/HomeScreen.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
   return (
     <View>
       <Text style={styles.text}>Andrew App</Text>
       <Button 
         title="Go to Components Demo" 
-        onPress={() => console.log('Button pressed!')}
+        onPress={() => props.navigation.navigate('Components')}
       />
-      <TouchableOpacity onPress={() => console.log('List pressed')}>
+      <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
         <Text>Go to List Demo</Text>
       </TouchableOpacity>
     </View>
