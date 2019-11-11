ca0d3444e367f7cd511ff60fc6b5dd7b28733b8b
Andrew Dinh <adinh@sightmachine.com>
2019-11-10 21:35:01 -0800

    Import and use SquareCounter component in Square Screen

--- rn-react/src/components/SquareCounter.js
+++ rn-react/src/components/SquareCounter.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 
-const SquareCounter = () => {
+const SquareCounter = ({ color }) => {
   return (
     <View>
-      <Text>Square Counter</Text>
+      <Text>{color}</Text>
+      <Button 
+        title={`More ${color}`}
+      />
+      <Button 
+        title={`Less ${color}`}
+      />
     </View>
   )
 }
--- rn-react/src/screens/SquareScreen.js
+++ rn-react/src/screens/SquareScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
+import SquareCounter from '../components/SquareCounter'
 
 const SquareScreen = () => {
   const [red, setRed] = useState(255);
@@ -9,45 +10,16 @@ const SquareScreen = () => {
 
   return (
     <View>
-      <Text>Red</Text>
-      <Button
-        title="More Red"
-        onPress={() => {
-          setRed((red + 1) % 255)
-        }}
+      <SquareCounter
+        color="Red"
       />
-      <Button
-        title="Less Red"
-        onPress={() => {
-          setRed((red - 1) % 255)
-        }}
+      <SquareCounter
+        color="Green"
       />
-      <Text>Green</Text>
-      <Button
-        title="More Green"
-        onPress={() => {
-          setGreen((green + 1) % 255)
-        }}
+      <SquareCounter
+        color="Blue"
       />
-      <Button
-        title="Less Green"
-        onPress={() => {
-          setGreen((green - 1) % 255)
-        }}
-      />
-      <Text>Blue</Text>
-      <Button
-        title="More Blue"
-        onPress={() => {
-          setBlue((blue + 1) % 255)
-        }}
-      />
-      <Button
-        title="Less Blue"
-        onPress={() => {
-          setBlue((blue - 1) % 255)
-        }}
-      />
+      
       <View
         style={{ height: 100, width: 100, backgroundColor: color }}
       />
