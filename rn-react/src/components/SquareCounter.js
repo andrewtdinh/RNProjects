cfe0760a88a8d2233cda50c39ba833325b29a818
Andrew Dinh <adinh@sightmachine.com>
2019-11-11 22:56:19 -0800

    Use function to make sure colors outside boundaries are not rendered

--- rn-react/src/components/SquareCounter.js
+++ rn-react/src/components/SquareCounter.js
@@ -4,16 +4,28 @@ import { View, Text, StyleSheet, Button } from 'react-native'
 const COLOR_CHANGE_AMOUNT = 15;
 
 const SquareCounter = ({ color, setColor, colorAmount }) => {
+
+  const calculateColor = (colorAmount, changeAmount) => {
+    const nextAmount = colorAmount + changeAmount;
+    if (nextAmount <= 0) {
+      return 0;
+    } else if (nextAmount >= 255) {
+      return 255;
+    } 
+
+    return nextAmount
+  }
+
   return (
     <View>
       <Text>{color}</Text>
       <Button 
         title={`More ${color}`}
-        onPress={() => setColor((colorAmount + COLOR_CHANGE_AMOUNT) % 255)}
+        onPress={() => setColor(calculateColor(colorAmount, COLOR_CHANGE_AMOUNT))}
       />
       <Button 
         title={`Less ${color}`}
-        onPress={() => setColor((colorAmount - COLOR_CHANGE_AMOUNT) % 255)}
+        onPress={() => setColor(calculateColor(colorAmount, -1 * COLOR_CHANGE_AMOUNT))}
       />
     </View>
   )
