91a11964aa890dd8d0d6060d3a636c3ae675502e
Andrew Dinh <adinh@sightmachine.com>
2019-11-11 21:49:43 -0800

    Create a constant to change the color by

--- rn-react/src/components/SquareCounter.js
+++ rn-react/src/components/SquareCounter.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
 
+const COLOR_CHANGE_AMOUNT = 15;
+
 const SquareCounter = ({ color, setColor, colorAmount }) => {
   return (
     <View>
       <Text>{color}</Text>
       <Button 
         title={`More ${color}`}
-        onPress={() => setColor((colorAmount + 10) % 255)}
+        onPress={() => setColor((colorAmount + COLOR_CHANGE_AMOUNT) % 255)}
       />
       <Button 
         title={`Less ${color}`}
-        onPress={() => setColor((colorAmount - 10) % 255)}
+        onPress={() => setColor((colorAmount - COLOR_CHANGE_AMOUNT) % 255)}
       />
     </View>
   )
