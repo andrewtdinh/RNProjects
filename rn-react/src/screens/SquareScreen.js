0b5b684aee9c55a7aaff6a599e1f8ab2457da78e
Andrew Dinh <adinh@sightmachine.com>
2019-11-15 08:08:00 -0800

    More refactoring of actions and dispatches

--- rn-react/src/screens/SquareScreen.js
+++ rn-react/src/screens/SquareScreen.js
@@ -7,7 +7,7 @@ const COLOR_CHANGE_AMOUNT = 15;
 const reducer = (state, action) => {
   // state is { red: number, green: number, blue: number }
   // action is { type: 'change_red' || 'change_green' || 'change_blue', payload: 15 || -15 }
-  switch (action.colorToChange) {
+  switch (action.type) {
     case 'change_red':
       if (state.red + action.payload > 255 || state.red + action.payload < 0) {
         return state;
@@ -39,18 +39,18 @@ const SquareScreen = () => {
     <View>
       <SquareCounter
         color="Red"
-        onIncrease={() => dispatch({colorToChange: 'red', amount: COLOR_CHANGE_AMOUNT})}
-        onDecrease={() => dispatch({ colorToChange: 'red', amount: -1 * COLOR_CHANGE_AMOUNT })}
+        onIncrease={() => dispatch({type: 'change_red', payload: COLOR_CHANGE_AMOUNT})}
+        onDecrease={() => dispatch({ type: 'change_red', payload: -1 * COLOR_CHANGE_AMOUNT })}
       />
       <SquareCounter
         color="Green"
-        onIncrease={() => dispatch({ colorToChange: 'green', amount: COLOR_CHANGE_AMOUNT })}
-        onDecrease={() => dispatch({ colorToChange: 'green', amount: -1 * COLOR_CHANGE_AMOUNT })}
+        onIncrease={() => dispatch({ type: 'change_green', payload: COLOR_CHANGE_AMOUNT })}
+        onDecrease={() => dispatch({ type: 'change_green', payload: -1 * COLOR_CHANGE_AMOUNT })}
       />
       <SquareCounter
         color="Blue"
-        onIncrease={() => dispatch({ colorToChange: 'blue', amount: COLOR_CHANGE_AMOUNT })}
-        onDecrease={() => dispatch({ colorToChange: 'blue', amount: -1 * COLOR_CHANGE_AMOUNT })}
+        onIncrease={() => dispatch({ type: 'change_blue', payload: COLOR_CHANGE_AMOUNT })}
+        onDecrease={() => dispatch({ type: 'change_blue', payload: -1 * COLOR_CHANGE_AMOUNT })}
       />
       
       <View
