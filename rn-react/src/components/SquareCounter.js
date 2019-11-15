358bf42da5a2190f55cbd9895c7b884f2326ca97
Andrew Dinh <adinh@sightmachine.com>
2019-11-14 23:50:02 -0800

    Remove redundant function in SquareCounter

--- rn-react/src/components/SquareCounter.js
+++ rn-react/src/components/SquareCounter.js
@@ -2,18 +2,6 @@ import React from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
 
 const SquareCounter = ({ color, onIncrease, onDecrease }) => {
-
-  const calculateColor = (colorAmount, changeAmount) => {
-    const nextAmount = colorAmount + changeAmount;
-    if (nextAmount <= 0) {
-      return 0;
-    } else if (nextAmount >= 255) {
-      return 255;
-    } 
-
-    return nextAmount
-  }
-
   return (
     <View>
       <Text>{color}</Text>
