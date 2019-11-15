b9e5509e6a3a311f3e04db0fda48964f1c11af9c
Andrew Dinh <adinh@sightmachine.com>
2019-11-14 23:58:43 -0800

    Add validation to prevent color change from exceeding limits

--- rn-react/src/screens/SquareScreen.js
+++ rn-react/src/screens/SquareScreen.js
@@ -9,11 +9,23 @@ const reducer = (state, action) => {
   // action is { colorToChange: 'red' || 'green' || 'blue', amount: 15 || -15 }
   switch (action.colorToChange) {
     case 'red':
+      if (state.red + action.amount > 255 || state.red + action.amount < 0) {
+        return state;
+      }
       return { ...state, red: state.red + action.amount }
+
     case 'green':
+      if (state.green + action.amount > 255 || state.green + action.amount < 0) {
+        return state;
+      }
       return { ...state, green: state.green + action.amount }
+
     case 'blue':
+      if (state.blue + action.amount > 255 || state.blue + action.amount < 0) {
+        return state;
+      }
       return { ...state, blue: state.blue + action.amount }
+
     default:
       return state;
   }
