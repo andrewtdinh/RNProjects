ca96f9592cd77389dbdc1da0729506e240a3fbeb
Andrew Dinh <adinh@sightmachine.com>
2019-11-19 23:24:37 -0800

    Add two more children to do flexbox

--- rn-react/src/screens/BoxScreen.js
+++ rn-react/src/screens/BoxScreen.js
@@ -4,7 +4,9 @@ import { View, Text, StyleSheet } from 'react-native'
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Box Screen</Text>
+      <Text style={styles.textStyle}>Child #1</Text>
+      <Text style={styles.textStyle}>Child #2</Text>
+      <Text style={styles.textStyle}>Child #3</Text>
     </View>
   )
 }
