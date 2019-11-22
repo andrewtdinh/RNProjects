11019489a5018b5a53022fd0227020bd21782ec6
Andrew Dinh <adinh@sightmachine.com>
2019-11-21 22:35:30 -0800

    Playing with children flex values

--- rn-react/src/screens/BoxScreen.js
+++ rn-react/src/screens/BoxScreen.js
@@ -4,23 +4,28 @@ import { View, Text, StyleSheet } from 'react-native'
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Child #1</Text>
-      <Text style={styles.textStyle}>Child #2</Text>
-      <Text style={styles.textStyle}>Child #3</Text>
+      <Text style={styles.textOneStyle}>Child #1</Text>
+      <Text style={styles.textTwoStyle}>Child #2</Text>
+      <Text style={styles.textThreeStyle}>Child #3</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   viewStyle: {
-    borderWidth: 1,
+    borderWidth: 3,
     borderColor: 'black',
-    flexDirection: 'row',
     height: 200,
-    justifyContent: 'center',
-    alignItems: 'center'
   },
-  textStyle: {
+  textOneStyle: {
+    borderWidth: 3,
+    borderColor: 'red',
+  },
+  textTwoStyle: {
+    borderWidth: 3,
+    borderColor: 'red',
+  },
+  textThreeStyle: {
     borderWidth: 3,
     borderColor: 'red',
   }
