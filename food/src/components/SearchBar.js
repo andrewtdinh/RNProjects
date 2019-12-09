246a04983caccd22a1a08b204437774e21fa9483
Andrew Dinh <adinh@sightmachine.com>
2019-12-08 21:27:30 -0800

    Create barebone SearchBar component

---
+++ food/src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchScreSearchBaren = () => {
+  return (
+    <View>
+      <Text>Search Bar</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default SearchBar;
