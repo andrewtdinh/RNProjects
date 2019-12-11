a7e25ab4ef21e9677bc0b4119cb6dc37756983d4
Andrew Dinh <adinh@sightmachine.com>
2019-12-10 23:08:46 -0800

    Use a Text Input in search bar and adjust style

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 const SearchBar = () => {
   return (
     <View style={styles.background}>
       <Feather name="search" size={30} />
-      <Text>Search Bar</Text>
+      <TextInput 
+        placeholder="Search"
+      />
     </View>
   )
 }
@@ -16,7 +18,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#F0EEEE',
     height: 50,
     borderRadius: 5,
-    marginHorizontal: 15
+    marginHorizontal: 15,
+    flexDirection: 'row'
   }
 })
 
