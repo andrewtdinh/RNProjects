a887ab89074e948ccad6cff58995f0cb5327ec93
Andrew Dinh <adinh@sightmachine.com>
2019-12-10 23:59:47 -0800

    Hook up onChangeText

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange }) => {
   return (
     <View style={styles.backgroundStyle}>
       <Feather name="search" style={styles.iconStyle} />
       <TextInput 
         placeholder="Search"
         style={styles.inputStyle}
+        value={term}
+        onChangeText={(newTerm) => onTermChange(newTerm)}
       />
     </View>
   )
